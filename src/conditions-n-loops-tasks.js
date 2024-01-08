e648693577f076ba741dd44ee83196aaa8717f10
yanasaa <yanasaa@gmail.com>
2024-01-08 19:43:46 +0300

    feat: add isIsoscelesTriangle func

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -94,8 +94,14 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b <= c || b + c <= a || c + a <= b) {
+    return false;
+  }
+  if (a === b || b === c || c === a) {
+    return true;
+  }
+  return false;
 }
 
 /**
